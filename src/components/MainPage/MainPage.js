281f51c6e7299df3f1fb77c4368c62a6275dbe5d
jlaprade33 <43327513+jlaprade33@users.noreply.github.com>
2019-12-10 20:24:44 -0700

    final photo size update

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -71,7 +71,7 @@ const styles = theme => ({
         position: 'relative',
         boxShadow: '2px 2px 2px 0px black',
         [theme.breakpoints.down('xs')]:{
-            maxHeight: 200,
+            maxHeight: 250,
         },
         [theme.breakpoints.up('sm')]:{
             maxHeight: 500,
