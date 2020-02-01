6bf913e340b9bbec0d783c0a0417f2c94c9a19c6
Jeff LaPrade <jeffrey.laprade@gmail.com>
2020-02-01 14:49:18 -0700

    bcakground table done

--- src/components/BackgroundTable/components/Background.js
+++ src/components/BackgroundTable/components/Background.js
@@ -45,7 +45,17 @@ const styles = theme => ({
   buttonContainer: {
     width: '90%',
     display:'table-flex',
-    border: '1px solid orange',
+    borderTop: '2px solid #696969'
+  },
+  buttonContainer: {
+    width: '90%',
+    display:'table-flex',
+    borderTop: '2px solid #696969'
+  },
+  carousel: {
+    columnCount: 3,
+    width: '90%',
+    display:'table-flex',
     borderTop: '2px solid #696969'
   },
   buttons: {
@@ -55,6 +65,24 @@ const styles = theme => ({
     textAlign: 'center',
     minHeight: 50,
     paddingTop: 10
+  },
+  buttonUnderline: {
+    width: "fit-content",
+    display: 'table-flex',
+    margin: 'auto',
+    textAlign: 'center',
+    minHeight: 50,
+    paddingTop: 10,
+    borderBottom: '3px solid #25274b'
+  },
+  oneButton: {
+    width: "fit-content",
+    display: 'inline-flex',
+    margin: 'auto',
+    textAlign: 'center',
+    minHeight: 40,
+    margin: '10px 40px 0 40px',
+    borderBottom: '3px solid #25274b'
   },
   innerContent: {
     width: "100%"
@@ -70,7 +98,8 @@ class Background extends Component {
   state = {
     title: 'My background',
     content: ['Check out my a quick summary of my background by clicking through this table'],
-    width: 0
+    width: 0,
+    index: 0
   }
 
   componentDidMount(){
@@ -86,12 +115,23 @@ class Background extends Component {
     this.setState({ width: window.innerWidth})
   }
 
-  updateContent = (inputArray) => {
+  updateContent = (index, inputArray) => {
     console.log("content", inputArray)
     this.setState({
       title: inputArray.title,
-      content: inputArray.display
+      content: inputArray.display,
+      index
     })
+  }
+
+  scroll = (side, array) => {
+    let currentIndex = this.state.index
+    if(side === 'left'){
+      this.state.index > 0 ? this.updateContent(currentIndex - 1, array[currentIndex - 1]) : this.updateContent(currentIndex, array[currentIndex])
+    }
+    else {
+      this.state.index < 3 ? this.updateContent(currentIndex + 1, array[currentIndex + 1]) : this.updateContent(currentIndex, array[currentIndex])
+    }
   }
 
   render() {
@@ -132,16 +172,24 @@ class Background extends Component {
         </div>
         <div className={classes.contentContainer}>
           {
+            this.state.width > 923 ? (
             backgroundInfo.map((tableContent, index) => {
               return (
-                  <div  key={index} className={classes.buttonContainer}>
-                    <div className={classes.buttons} onClick={() => this.updateContent(backgroundInfo[index])}>{ tableContent.title }</div>
-                  </div>
+                <div  key={index} className={classes.buttonContainer}>
+                  <div className={this.state.index === index ? classes.buttonUnderline : classes.buttons} onClick={() => this.updateContent(index, backgroundInfo[index])}>{ tableContent.title }</div>
+                </div>
               )
             })
+            ) : (
+              <div className={classes.buttonContainer}>
+                <svg onClick={()=> this.scroll('left', backgroundInfo) } xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/> </svg> 
+                <p className={classes.oneButton}>{ backgroundInfo[this.state.index].title }</p>
+                <svg onClick={()=> this.scroll('right', backgroundInfo) } xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>
+              </div>
+            )
           }
-         </div>
       </div>
+    </div>
     )
   }
 }
